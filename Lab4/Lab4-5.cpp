ebad9a5bc9aa91262a01c824983a9fb337ad4bd0
Supapong-P <supapong.pasmanee@gmail.com>
2025-12-23 15:16:29 +0700

    Update array and bounds for new values

--- Lab4/Lab4-5.cpp
+++ Lab4/Lab4-5.cpp
@@ -3,13 +3,13 @@
 using namespace std;
 
 int main() {
-    int arr[] = {1, 3, 3, 3, 5, 7};
+    int arr[] = {1, 3, 3, 3, 5, 7 , 9 , 9 , 10 , 10};
 
-    int* lb = lower_bound(begin(arr)+ end(arr), 3);
-    int* ub = upper_bound(begin(arr) + end(arr), 3);
+    int* lb = lower_bound(begin(arr)+ end(arr), 9);
+    int* ub = upper_bound(begin(arr) + end(arr), 9);
 
-    cout << "First 3 at index " << (lb - arr) << endl;
-    cout << "Count of 3 = " << (ub - lb);
+    cout << "First 9 at index " << (lb - arr) << endl;
+    cout << "Count of 9 = " << (ub - lb);
 
     return 0;
 }
